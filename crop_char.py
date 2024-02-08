74367b7d834be087d0c840677980c81e7ce5ba9f
AllenHeartcore <ziyuanc3@illinois.edu>
2024-02-08 12:42:59 +0800

    Auto-crop characters (denoise 2: ignore background noise)

--- crop_char.py
+++ crop_char.py
@@ -9,22 +9,21 @@ for r, ds, fs in os.walk('Character'):
 
             img = Image.open(os.path.join(r, f))
             arr = np.array(img)
-            alpha = arr[:, :, 3]
+            alpha = np.maximum(arr[:, :, 3], 2) - 2
             w, h = img.size
 
             sum_h = np.sum(alpha, axis=0)
-            lim_l = np.argmax(sum_h > 0)
+            lim_l, lim_r = 0, 0
             while sum_h[lim_l + 1] == 0:
                 sum_h[lim_l] = 0
                 lim_l = np.argmax(sum_h > 0)
-            lim_r = np.argmax(sum_h[::-1] > 0)
             while sum_h[w - lim_r - 2] == 0:
                 sum_h[w - lim_r - 1] = 0
                 lim_r = np.argmax(sum_h[::-1] > 0)
             lim_h = np.minimum(lim_l, lim_r)
 
             sum_v = np.sum(alpha, axis=1)
-            lim_v = np.argmax(sum_v > 0)
+            lim_v = 0
             while sum_v[lim_v + 1] == 0:
                 sum_v[lim_v] = 0
                 lim_v = np.argmax(sum_v > 0)
